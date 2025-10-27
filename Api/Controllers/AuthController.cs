7783112fc75266e0deeeb71c476f354bd6986c17
Quang Anh <144104822+QuangAnh2109@users.noreply.github.com>
2025-10-27 10:04:30 +0700

    Add AuthController

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class AuthController : ControllerBase
     {
     }
 }
